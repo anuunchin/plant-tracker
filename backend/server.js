6112111077741ac80f176cbda5eb42b701109405
anuunchin <anuun.chinbat@campus.tu-berlin.de>
2024-11-11 19:44:50 +0100

    Everything for issue 1

--- backend/server.js
+++ backend/server.js
@@ -3,23 +3,49 @@ const {v4: uuidv4 } = require("uuid");
 const app = express()                               //creates express app
 const PORT = 3000;                                  ///middleware func that processes requests before they reach route handlers
 
+const fs = require('fs');
+const path = require('path');
+const dataFilePath = path.join(__dirname, 'plants.json');
+
+function loadPlants() {
+    try {
+        const data = fs.readFileSync(dataFilePath, 'utf8');
+        return JSON.parse(data);
+    } catch (err) {
+        return [];
+    }
+}
+
+function savePlants(data) {
+    fs.writeFileSync(dataFilePath, JSON.stringify(data, null, 2), 'utf8');
+}
+
+
+let plants = loadPlants();
+
 app.use(express.json());                            //converts incoming json data to js objects
 
 app.listen(PORT, () => {                            //() => { ... } is a callback on successful start
     console.log(`Server is running on http://localhost:${PORT}`)
 });
 
-let plants = [];                
 
 app.post('/plants', (req, res) => {                 //handles POST requests to the /plants endpoint
-    const { name, lastWatered} = req.body;
+    const { name, lastWatered, wateringInterval} = req.body;
 
     if (!lastWatered || isNaN(Date.parse(lastWatered))) {
         return res.status(400).send({ error: 'Invalid or missing lastWatered date' });
     }
 
-    const plant = { name, lastWatered, id: uuidv4(), wateringHistory: [lastWatered]}; 
+    if (!wateringInterval || isNaN(wateringInterval) || wateringInterval <= 0) {
+        return res.status(400).send({ error: 'Invalid or missing watering interval' });
+    }
+
+    const plant = { name, lastWatered, id: uuidv4(), wateringHistory: [lastWatered], wateringInterval}; 
     plants.push(plant);                             //equals to append in Python
+   
+    savePlants(plants);
+
     res.status(201).send(plant);                    //sends success status with plant object
 });
 
@@ -51,6 +77,9 @@ app.patch('/plants/:id/water', (req, res) => {
         plant.wateringHistory.push(lastWateredDate);
         // Order list so that history looks clean
         plant.wateringHistory.sort((a,b) => new Date(b) - new Date(a));
+
+        savePlants(plants);
+
         res.send(plant);
     } else {
         return res.status(400).send({error: 'No such plant found.'})
@@ -76,7 +105,11 @@ app.put('/plants/:id', (req, res) => {              //handles PUT requests based
 app.delete('/plants/:id', (req, res) => {           //handles DELETE requests based on plant id
     const { id } = req.params;
     plants = plants.filter((p) => p.id !== id);     //compared to find, returns all matching objects
+   
+    savePlants(plants);
+
     res.status(204).send(); 
 });
 
 app.use(express.static('frontend'));
+
